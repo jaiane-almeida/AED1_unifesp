ca51472fa48f64e9c9e12f97820c376a4d0edd5a
Jaiane Almeida <108701893+jaiane-almeida@users.noreply.github.com>
2025-09-23 20:52:11 -0300

    Create 1180.alocacao.c
    Exercicio 1180 comalocação dinamica

---
+++ 1180.alocacao.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h> // necessário para malloc e free
+
+int main() {
+    int n, menor, posicao; 
+    printf("Digite os numeros que voce quer: ");
+    scanf("%d", &n);
+
+    // aloca dinamicamente memória para 'n' inteiros
+    int *vetor = (int *) malloc(n * sizeof(int));
+
+  // preenchendo o vetor
+    for (int i = 0; i < n; i++) {
+      scanf("%d", &vetor[i]);
+    }
+
+    if (vetor == NULL) { //Você precisa verifica r antes para evitar acessar memória inválida.
+        printf("Erro: memória não alocada!\n");
+        return 1; // encerra o programa
+    }
+
+    // mostrando o vetor só pra testar
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+
+    menor = vetor[0];
+    posicao = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (vetor[i] < menor) {
+            menor = vetor[i];
+            posicao = i; 
+        }
+    }
+
+    printf("Menor valor: %d", menor);
+    printf("\nPosicao: %d\n", posicao +1);
+
+    // libera a memória alocada
+    free(vetor);
+
+    return 0;
+}
+
