3eb652b12e89e225aa26e6b5a3b732521562fd27
Jaiane Almeida <108701893+jaiane-almeida@users.noreply.github.com>
2025-11-28 17:41:30 -0300

    Add program to calculate distance using velocity and time

---
+++ 1197.c
@@ -0,0 +1,11 @@
+//voltando para faculdade de fisica 
+
+#include <stdio.h>
+
+int main() {
+    int v, t;
+    while (scanf("%d %d", &v, &t) != EOF) {
+        printf("%d\n", 2 * v * t);
+    }
+    return 0;
+}
